2662a0f75b8ceb7ac35e8543a7c3049ea8e6d279
Chrysa Lazaridou <chrysanthi.lazaridou@gmail.com>
2020-12-16 03:26:41 +0200

    fourth and fifth exercises and style application

--- script.js
+++ script.js
@@ -31,17 +31,67 @@
     const fileType = filePath.slice(indexNumber);
     
     document.getElementById("result2").innerHTML = fileType;
-    
  }
 
      
 /* Exercise 3: Write a JavaScript program to replace every character in a
         given string with the character following it in the alphabet */
+    function nextLetter() {
+       const text = document.getElementById("randomString1").value.toLowerCase().split("");
+       const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+       let x ="";
+        
+       for (let i = 0; i < text.length; i++ ){
+           for (let j=0 ; j < alphabet.length ; j++) {
+               if (text[i] === alphabet[j] ) {
+                x += text.replace(text[i] , alphabet[j+1]);
+                console.log(x);
+                // x += alphabet[j+1];
+               }
+           }
+          
+
+       }
+        //console.log(x);
+    
+    }
+    
       
 /* Exercise 4: Write a JavaScript program to get the current date. Expected
         Output : mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy */
      
+        function today() {
+
+
+        const date = new Date();
+        const day = date.getDate();
+        const year = date.getFullYear();
+        const month =date.getMonth() + 1;
+        
+
+        const fullday = (day < 10)? `0${day}`:`${day}` ;
+        const fullmonth = (month < 10)? `0${month}`:`${month}` ;
+
+        document.getElementById("date").innerHTML = `${fullmonth}-${fullday}-${year} , ${fullmonth}/${fullday}/${year}`;
+    }
+      today();
+
+
 /* Exercise 5: Write a JavaScript program to create a new string adding
         "New!" in front of a given string. If the given string begins with
         "New!" already then return the original string */
      
+        function randomText() {
+            const randtext = document.getElementById("randomString2").value ;
+           
+
+            if (randtext.slice(0,4) === "New!") {
+
+               document.getElementById("result5").innerHTML = randtext;
+             
+            }
+            else {
+
+               document.getElementById("result5").innerHTML= `New! ${randtext}`;
+            }
+        }
