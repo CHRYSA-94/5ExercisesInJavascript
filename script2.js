c1b200678bbfa6d3fee30fe7e77c066e2b8398b1
Costas-Zeimpekis <morpheustudio@gmail.com>
2020-12-18 14:49:19 +0200

    review: solutions

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-
+// Nice colors
  /*Exercise 1: Write a JavaScript program to find the number of even digits
   in a an array of integers*/
     function ex1(){
@@ -7,7 +7,9 @@
       document.getElementById("result1").innerHTML = num1.filter(n => Number(n) % 2 === 0 ).length;
       
     } 
-  
+
+//Costas
+// Good work 
 
 /*Exercise 2: Write a JavaScript program to find the number of even values
    up to a given number*/
@@ -25,7 +27,11 @@
      
       document.getElementById("result2").innerHTML = finalArr.length;
    }
-   
+
+   //Costas
+   // Nice work
+
+
 /* Exercise 3: Write a JavaScript program to check whether a given array of
    integers is sorted in ascending order*/
 
@@ -33,10 +39,16 @@
      
     const num3 = document.getElementById("num3").value.split(",");
     const sortNum3 = num3.map(n => n).sort((a, b) => {return Number(a) - Number(b)});
+    // Why use map 
     
     document.getElementById("result3").innerHTML = (num3.join() === sortNum3.join()) ? "true":"false"
     
   }
+
+
+  //Costas
+  // Rethink what you are doing
+  const isAscending = (arr) => arr.every((a, i) => arr[i + 1] > a);
       
  /* Exercise 4: Write a JavaScript program to get the largest even number
   from an array of integers.*/
@@ -47,6 +59,11 @@
     const sortArray = num4.filter(n => n % 2 == 0).sort((a, b) => {return Number(a) - Number(b)});
     document.getElementById("result4").innerHTML = sortArray.slice(sortArray.length-1);
   }
+
+
+//Costas
+const largestEven = (arr) => Math.max(...arr.filter(num => num % 2 === 0)); 
+
 /* Exercise 5: Write a JavaScript program to replace the first digit in a
    string (should contain at least one digit) with a $ character.*/
 
@@ -60,3 +77,8 @@
     document.getElementById("result5").innerHTML = (str5.length == 1)? `${str5}`:`${nStr.join().replace(/,/g,"")}`
 
   };
+
+  //Costas
+  const replaceFirstDigit = (str) => str.replace(/[0-9]/, '$');
+
+   
